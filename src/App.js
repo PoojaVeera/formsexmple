c5f2b384d3b0dc0c3bd3dc6912ed45b8fd2d0637
PoojaVeera <110709890+PoojaVeera@users.noreply.github.com>
2022-09-02 09:57:17 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useFormik } from "formik";
 
 function App() {
+  const form = useFormik({
+    initialValues: { email: "", password: "" },
+    onSubmit: (values) => {
+      console.log(values);
+      alert(JSON.stringify(values, null, 2));
+    },
+    validate: (values) => {
+      let errors = {};
+      if (!values.email) {
+        errors.email = "email required";
+      }
+      if (!values.password) {
+        errors.password = "password required";
+      }
+      return errors;
+    },
+  });
+  // console.log(f);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <form onSubmit={form.handleSubmit}>
+          <label htmlFor="email">EMAIL</label>
+          <br></br>
+          <input
+            type="email"
+            name="email"
+            onChange={form.handleChange}
+            value={form.values.email}
+          ></input>
+          {form.errors.email ? <div>{form.errors.email}</div> : null}
+          <br></br>
+          <label htmlFor="password">PASSWORD</label>
+          <br></br>
+          <input
+            type="password"
+            name="password"
+            onChange={form.handleChange}
+            value={form.values.password}
+          ></input>
+          {form.errors.password ? <div>{form.errors.password}</div> : null}
+          <br></br>
+          <button type="submit">Submit</button>
+        </form>
+      </center>
     </div>
   );
 }
